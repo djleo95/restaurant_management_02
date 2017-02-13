51a88f43a22e7ea3aa96f7f7d3f4141ada0dcc48
Strawberry <strawberrycsgo@gmail.com>
2017-02-13 11:18:23 +0700

    Sign up screen

--- restaurant_management/config/routes.rb
+++ restaurant_management/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root "pages#show", page: "home"
   get "/pages/:page" => "pages#show"
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  resources :users, only: [:show, :new, :create]
 end
