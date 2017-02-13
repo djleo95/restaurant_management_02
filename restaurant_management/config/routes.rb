338de127a46025640ad6569bacebf0463962061e
Strawberry <strawberrycsgo@gmail.com>
2017-02-13 11:18:23 +0700

    Login

--- restaurant_management/config/routes.rb
+++ restaurant_management/config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get "/pages/:page" => "pages#show"
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   resources :users, only: [:show, :new, :create]
 end
