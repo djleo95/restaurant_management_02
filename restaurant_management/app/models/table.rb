9086690a2170f2d583e58fd2d6b9d921f784e1d6
“LePhuc” <lephuc@Les-MacBook-Pro.local>
2017-02-15 10:12:34 +0700

    Find tables

--- restaurant_management/app/models/table.rb
+++ restaurant_management/app/models/table.rb
@@ -1,3 +1,9 @@
 class Table < ApplicationRecord
   has_many :order_tables
+
+  scope :free, -> (capacity, from, to) {
+    where(capacity: capacity)
+    .where( "id not in (?)",
+      (OrderTable.book_in_about(from ,to).map(&:table_id)))
+  }
 end
