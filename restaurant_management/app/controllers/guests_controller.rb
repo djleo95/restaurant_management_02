4cc3269b8134a118c2f5fa7f312799355cf872be
“LePhuc” <lephuc@Les-MacBook-Pro.local>
2017-02-23 23:25:23 +0700

    Review

--- restaurant_management/app/controllers/guests_controller.rb
+++ restaurant_management/app/controllers/guests_controller.rb
@@ -2,9 +2,17 @@ class GuestsController < ApplicationController
   def create
     @guest = Guest.new guest_params
     if @guest.save
-      redirect_to new_guest_order_table_path @guest
+      if params[:guest][:review_attributes]  
+        redirect_to reviews_path
+      else
+        redirect_to new_guest_order_table_path @guest
+      end
     else
-      render :new
+      if params[:guest][:review_attributes]
+        redirect_to new_review_path
+      else
+        render :new
+      end
     end
   end
 
@@ -14,6 +22,6 @@ class GuestsController < ApplicationController
 
   private
   def guest_params
-    params.require(:guest).permit :name, :email, :phone
+    params.require(:guest).permit :name, :email, :phone, review_attributes: [:content]
   end
 end
